9d503dd73dc90130de9ac9ece5548ecac886b58b
RXJpaw== <github@rxj.pw>
2022-06-18 03:39:33 +0200

    oops, pushed .js files

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-var vue_1 = require("vue");
-var App_vue_1 = __importDefault(require("./App.vue"));
-(0, vue_1.createApp)(App_vue_1["default"]).mount('#app');
